75fa6ea81971ecff9d2690e563e1d5f3368ea070
ShuklaDevRatan <panditdevratanshukla@gmail.com>
2026-07-03 22:14:10 +0530

    Time: 3 ms (18.95%), Space: 47.9 MB (37.93%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums.length <= 1) return;
+
+        int i = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+
+            if (nums[i] != 0) {
+                i++;
+            } else if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+}
