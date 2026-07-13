fe08cd1125cbad4edf41bb9c4449433e43c1f1ce
ShuklaDevRatan <panditdevratanshukla@gmail.com>
2026-07-13 08:56:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (73.12%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -3,35 +3,20 @@ class Solution {
 
         List<Integer> ans = new ArrayList<>();
 
+        String digits = "123456789";
+
         int startLength = String.valueOf(low).length();
-        int endLength = String.valueOf(high).length();
-        endLength = Math.min(endLength, 9);
+        int endLength = Math.min(String.valueOf(high).length(), 9);
 
         for (int len = startLength; len <= endLength; len++) {
 
-            // First sequential number (123..., length = len)
-            int num = 0;
-            for (int i = 1; i <= len; i++) {
-                num = num * 10 + i;
-            }
+            for (int i = 0; i <= 9 - len; i++) {
 
-            // Difference = 11, 111, 1111...
-            int diff = 0;
-            for (int i = 0; i < len; i++) {
-                diff = diff * 10 + 1;
-            }
-
-            while (true) {
+                int num = Integer.parseInt(digits.substring(i, i + len));
 
                 if (num >= low && num <= high) {
                     ans.add(num);
                 }
-
-                // Last digit is 9 -> no more sequential numbers of this length
-                if (num % 10 == 9)
-                    break;
-
-                num += diff;
             }
         }
 
