e0b58a21121eef9db02ae3bac304ec617025c01b
ShuklaDevRatan <panditdevratanshukla@gmail.com>
2026-07-21 12:39:56 +0530

    Time: 0 ms (100%), Space: 43.2 MB (33.27%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -4,7 +4,8 @@ class Solution {
 
         int low = 0;
         int high = 0;
-        
+
+        // Search Space -> [max element, total sum]
         for (int num : nums) {
             low = Math.max(low, num);
             high += num;
@@ -12,11 +13,14 @@ class Solution {
 
         while (low <= high) {
 
+            // Candidate answer
             int mid = low + (high - low) / 2;
 
             if (canSplit(nums, k, mid)) {
+                // Possible -> Try smaller answer
                 high = mid - 1;
             } else {
+                // Not possible -> Increase answer
                 low = mid + 1;
             }
         }
@@ -34,11 +38,13 @@ class Solution {
             if (currentSum + num <= maxAllowedSum) {
                 currentSum += num;
             } else {
+                // Start a new subarray
                 subarrays++;
                 currentSum = num;
             }
         }
 
+        // Greedy creates minimum subarrays needed
         return subarrays <= k;
     }
 }
