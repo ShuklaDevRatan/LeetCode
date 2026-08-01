2345208900638ceedc3fabdf32f7ec429d06efc6
ShuklaDevRatan <panditdevratanshukla@gmail.com>
2026-08-01 20:28:21 +0530

    Time: 0 ms (100%), Space: 43.3 MB (48.69%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -5,8 +5,50 @@ class Solution {
             return false;
         }
 
-        String doubled = s + s;
+        int i = 0;
 
-        return doubled.contains(goal);
+        while (i < s.length()) {
+
+            if (s.charAt(i) == goal.charAt(0)) {
+
+                int j = i;
+                int k = 0;
+
+                while (k < goal.length()) {
+
+                    if (s.charAt(j) != goal.charAt(k)) {
+                        break;
+                    }
+
+                    j++;
+                    k++;
+
+                    if (j == s.length()) {
+                        j = 0;
+                    }
+                }
+
+                if (k == goal.length()) {
+                    return true;
+                }
+            }
+
+            i++;
+        }
+
+        return false;
     }
 }
+
+
+
+// class Solution {
+//     public boolean rotateString(String s, String goal) {
+//         if (s.length() != goal.length()) {
+//             return false;
+//         }
+//         String doubled = s + s;
+//         return doubled.contains(goal);
+//     }
+// }
+// yeh optimal apprch h lekin isme contain ka use kiya h isslye upar wali logic wise zyada achhi h
