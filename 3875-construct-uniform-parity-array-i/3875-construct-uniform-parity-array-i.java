7a74ea15c8a294b9bfc5b4518db0fb3cd7dc1b49
ShuklaDevRatan <panditdevratanshukla@gmail.com>
2026-09-02 08:59:48 +0530

    Time: 1 ms (31.89%), Space: 45 MB (87.6%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,13 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
+        int oddCount = 0;
+
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                oddCount++;
+            }
+        }
+
         return true;
     }
 }
